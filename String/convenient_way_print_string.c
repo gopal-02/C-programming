332d6198c390a65b7a32d5d16c5c8d0d4a80c501
gopal8220 <gopalkumar8220@gmail.com>
2022-11-10 20:01:57 +0530

    initial commit

---
+++ String/convenient_way_print_string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    // int a = 4;
+    // printf("%d", a);
+
+    // char *ptr = "Gopal Kumar";
+    char ptr[] = "Gopal Kumar";
+    printf("%s", ptr);
+    return 0;
+}
